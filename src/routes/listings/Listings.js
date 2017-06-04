dd112dc349ac299a8a590bebb2e1c3db56346750
psyanite <psyanite@gmail.com>
2017-06-04 23:16:47 +1000

    Start reworking listings

--- src/routes/listings/Listings.js
+++ src/routes/listings/Listings.js
@@ -5,7 +5,6 @@ import s from './Listings.css';
 
 class Listings extends React.Component {
   static propTypes = {
-    title: PropTypes.string.isRequired,
     listings: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
@@ -14,21 +13,19 @@ class Listings extends React.Component {
 
   render() {
     return (
-      <div className={s.root}>
-        <div className={s.container}>
-          <h1>{this.props.title}</h1>
-          <p className={s.lead}>MEOW MEOW MEOW MEOW.</p>
-          {this.props.listings.map(item => (
-            <div key={item.id} className={s.newsItem}>
-              <h1 className={s.newsTitle}>{item.name}</h1>
-              <p>${item.price} AUD</p>
-              <p>{item.guestCount} Guests</p>
-              <p>{item.bedroomCount} Bedrooms</p>
-              <p>{item.bedCount} Beds</p>
-              <p>{item.description}</p>
-              <img className={s.picture} src={item.image} alt={item.name} />
-            </div>
-          ))}
+      <div className={s.lWrap}>
+        <div className={s.lFloor}>
+        {this.props.listings.map(item => (
+          <div key={item.id} className={s.lTile}>
+            <h1 className={s.title}>{item.id} {item.name}</h1>
+            <p>${item.price} AUD</p>
+            <p>{item.guestCount} Guests</p>
+            <p>{item.bedroomCount} Bedrooms</p>
+            <p>{item.bedCount} Beds</p>
+            <p>{item.description}</p>
+            <img className={s.picture} src={item.image} alt={item.name} />
+          </div>
+        ))}
         </div>
       </div>
     );
