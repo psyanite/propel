93d9d56aabde5c422554e591ca8dd69e9a42c6c0
psyanite <psyanite@gmail.com>
2017-06-22 23:57:33 +1000

    Add new route to retrieve one listings

---
+++ src/routes/listings/one/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Meowout from '../../../components/Meowout/Meowout';
+import Listings from './Listings';
+
+export default {
+
+  path: '/one',
+
+  async action({ fetch }) {
+    const resp = await fetch('/graphql', {
+      method: 'POST',
+      body: JSON.stringify({
+        query: '{allListings(orderBy:ID_ASC){edges{node{id,name,price,guestCount,bedroomCount,bedCount,image,description}}}}',
+      }),
+      headers: new Headers(),
+    });
+    const { data } = await resp.json();
+    if (!data) {
+      throw new Error('Ayy Lmeow1!!!');
+    }
+    return {
+      component: <Meowout><Listings listings={data.allListings.edges} /></Meowout>,
+    };
+  },
+
+};
