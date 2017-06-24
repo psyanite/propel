3f064e7a98299962d2e006e7da7d1eb5ba963a3a
psyanite <psyanite@gmail.com>
2017-06-24 15:58:21 +1000

    Update eslintrc to change max line length to 150

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 // ESLint configuration
 // http://eslint.org/docs/user-guide/configuring
 module.exports = {
@@ -85,5 +76,8 @@ module.exports = {
         moduleDirectory: ['node_modules', 'src'],
       },
     },
+
+    // Maximum length for file
+    'max-len': [1, 150, 4]
   },
 };
