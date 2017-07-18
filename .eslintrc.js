e56cfe1ea8fcc621149829f00a195d340a69bab7
psyanite <psyanite@gmail.com>
2017-07-18 23:34:18 +1000

    Update if-else brace style for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-// ESLint configuration
-// http://eslint.org/docs/user-guide/configuring
 module.exports = {
     parser: 'babel-eslint',
 
@@ -62,6 +51,14 @@ module.exports = {
       // You can reopen this if you still want this rule
       'react/prefer-stateless-function': 'off',
 
-      'max-len': [1, 1500, 4]
+      'max-len': [1, 1500, 4],
+
+      'brace-style': [
+        "error",
+        "stroustrup",
+        {
+          allowSingleLine: true
+        }
+      ],
     },
   };
