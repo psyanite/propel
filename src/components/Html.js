d038a505e71af7f5e5f61759a250764c4d0fb55a
psyanite <psyanite@gmail.com>
2017-07-30 22:58:03 +1000

    Add new font 'Dosis'

--- src/components/Html.js
+++ src/components/Html.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript';
@@ -44,6 +35,7 @@ class Html extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
           <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Dosis:500,700" rel="stylesheet" />
           {styles.map(style =>
             <style
               key={style.id}
