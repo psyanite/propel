61063b1ff4e58f80f365da08e42e3f8cd8956f11
psyanite <psyanite@gmail.com>
2017-08-03 02:11:46 +1000

    Update sequelize to use multiple parameters instead of connection string

--- src/data/sequelize.js
+++ src/data/sequelize.js
@@ -1,18 +1,19 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import Sequelize from 'sequelize';
 import config from '../config';
 
-const sequelize = new Sequelize(config.databaseUrl, {
+// const sequelize = new Sequelize('postgres://postgres:meow@localhost:5432/react-meow', {
+//   define: {
+//     freezeTableName: true,
+//     timestamps: false,
+//   },
+// });
+
+const sequelize = new Sequelize('react-meow', 'postgres', 'meow', {
+  host: 'localhost',
+  dialect: 'postgres',
   define: {
     freezeTableName: true,
+    timestamps: false,
   },
 });
 
