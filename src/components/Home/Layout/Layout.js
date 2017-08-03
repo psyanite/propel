8a8845c2b42c85242a5f587ab24e86ba9f7bf0fb
psyanite <psyanite@gmail.com>
2017-08-03 23:41:49 +1000

    Remove unused reference Footer in Home Layout

--- src/components/Home/Layout/Layout.js
+++ src/components/Home/Layout/Layout.js
@@ -3,7 +3,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Layout.css';
-import Footer from '../Footer';
 
 class Layout extends React.Component {
   static propTypes = {
@@ -12,10 +11,7 @@ class Layout extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.props.children}
-        <Footer />
-      </div>
+      <div> {this.props.children} </div>
     );
   }
 }
