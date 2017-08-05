1bf7614a34e1d6df8e1bf2564645e6e2908a5fc2
psyanite <psyanite@gmail.com>
2017-08-05 14:14:27 +1000

    Add filters for area and property type onto homepage

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -4,7 +4,7 @@ import Layout from '../../components/Home/Layout';
 import Home from './Home';
 
 async function action({ fetch }) {
-  const allAreasQuery = graphqlify({
+  const filterOptionsQuery = graphqlify({
     allAreas: {
       fields: {
         nodes: {
@@ -17,7 +17,7 @@ async function action({ fetch }) {
   });
   const resp = await fetch('/graphql', {
     method: 'POST',
-    body: JSON.stringify({ query: allAreasQuery }),
+    body: JSON.stringify({ query: filterOptionsQuery }),
     headers: new Headers(),
   });
   const { data } = await resp.json();
@@ -25,21 +25,18 @@ async function action({ fetch }) {
     throw new Error('Meow');
   }
 
-  // TODO: Refactor areaOptions
-  const areas = data.allAreas.nodes
-    .map(node => node.area)
-    .reduce((x, y) => x.includes(y) ? x : [...x, y], [])
-    .reduce((acc, cur, i) => {
-      acc[i] = { value: cur, label: cur };
-      return acc;
-    }, {});
-  const areaOptions = Object.values(areas);
+  const filters = Object.keys(data).map((key) => {
+    const filter = {};
+    filter[key] = data[key].nodes.map(node => ({ name: node.name, value: node.name }));
+    filter[key].unshift({ name: 'Any', value: null });
+    return filter;
+  });
 
   return {
     chunks: ['home'],
     title: 'Meow',
     component: (
-      <Layout><Home areaOptions={areaOptions} /></Layout>
+      <Layout><Home filterOptions={filters} /></Layout>
     ),
   };
 }
