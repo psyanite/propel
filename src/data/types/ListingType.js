524c9ddd153bc3a77be10808b434a9441c1076d8
psyanite <psyanite@gmail.com>
2017-08-10 23:08:55 +1000

    Add PropertyType into ListingType

--- src/data/types/ListingType.js
+++ src/data/types/ListingType.js
@@ -5,10 +5,12 @@ import {
   GraphQLString as String,
 } from 'graphql';
 import { resolver } from 'graphql-sequelize';
-import { Listing, Area } from '../models';
+import { Listing, Area, PropertyType } from '../models';
 import AreaType from './AreaType';
+import PropertyTypeType from './PropertyTypeType';
 
 Listing.Area = Listing.belongsTo(Area);
+Listing.PropertyType = Listing.belongsTo(PropertyType);
 
 // todo: add comments and descriptions
 const ListingType = new ObjectType({
@@ -20,10 +22,15 @@ const ListingType = new ObjectType({
       type: AreaType,
       resolve: resolver(Listing.Area),
     },
+    propertyType: {
+      type: PropertyTypeType,
+      resolve: resolver(Listing.PropertyType),
+    },
     price: { type: new NonNull(Int) },
     bedroomCount: { type: new NonNull(Int) },
     guestCount: { type: new NonNull(Int) },
     bedCount: { type: new NonNull(Int) },
+    link: { type: new NonNull(String) },
     image: { type: new NonNull(String) },
     description: { type: new NonNull(String) },
   },
